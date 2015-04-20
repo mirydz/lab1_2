9395310ea861fc4bfda8d7e1ae9fe81530c61d67
Michal Rydz <mirydz@gmail.com>
2015-04-20 05:54:39 +0200

    Remove tax calculation logic from BookKeeper, refactor the class to use ITaxCalculator implementation

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeper.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeper.java
@@ -24,39 +24,14 @@ import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 public class BookKeeper {
 
-	public Invoice issuance(InvoiceRequest invoiceRequest) {
+	public Invoice issuance(InvoiceRequest invoiceRequest, ITaxCalculator taxCalculator) {
 		Invoice invoice = InvoiceFactory.generateInvoice(invoiceRequest);
 
 		for (RequestItem item : invoiceRequest.getItems()) {
 			Money net = item.getTotalCost();
-			BigDecimal ratio = null;
-			String desc = null;
-			
-			switch (item.getProductData().getType()) {
-			case DRUG:
-				ratio = BigDecimal.valueOf(0.05);
-				desc = "5% (D)";
-				break;
-			case FOOD:
-				ratio = BigDecimal.valueOf(0.07);
-				desc = "7% (F)";
-				break;
-			case STANDARD:
-				ratio = BigDecimal.valueOf(0.23);
-				desc = "23%";
-				break;
-				
-			default:
-				throw new IllegalArgumentException(item.getProductData().getType() + " not handled");
-			}
-					
-			Money taxValue = net.multiplyBy(ratio);
-			
-			Tax tax = new Tax(taxValue, desc);
-			
 
 			InvoiceLine invoiceLine = new InvoiceLine(item.getProductData(),
-					item.getQuantity(), net, tax);
+					item.getQuantity(), net, taxCalculator.calculateTax(item));
 			invoice.addItem(invoiceLine);
 		}
 
